242f974d3c26b2f9d497d8389def79f0ea0d03ec
Dmitry Yakimov <welldan97@gmail.com>
2015-11-22 04:51:38 +0300

    Building function with arguments

--- src/apple-java-script.js
+++ src/apple-java-script.js
@@ -1,11 +1,18 @@
+import _ from 'lodash';
+import serialize from 'serialize-javascript';
+
 let AppleJavaScript = () => {
   // 1. build function
   // 2. exec
   // 3. eval result
 };
 
-AppleJavaScript.buildFunction = (fn) => {
-  return `(${fn.toString()})()`;
+AppleJavaScript.buildFunction = (...args) => {
+  let fn = _(args).last()
+  let argsToPass = args
+        .slice(0, args.length - 1)
+        .map( v => serialize(v)).join(', ');
+  return `(${fn.toString()})(${argsToPass})`;
 };
 
 export default AppleJavaScript;
--- test/apple-java-script-test.js
+++ test/apple-java-script-test.js
@@ -1,10 +1,8 @@
 /*global describe it beforeEach Application */
 import { expect } from 'chai';
-import AppleJavaScript from '../src/apple-java-script';
+import ajs from '../src/apple-java-script';
 
 describe('AppleJavaScript', () => {
-  let ajs = AppleJavaScript;
-
   describe('.build', () => {
     it('makes text string out of function', () => {
       let functionText = ajs.buildFunction(() => {
@@ -12,6 +10,16 @@ describe('AppleJavaScript', () => {
       });
       expect(functionText).to.equal(
         "(function () {\n        return 5 + 5;\n      })()"
+      );
+    });
+
+    it('inserts arguments', () => {
+      let functionText = ajs.buildFunction(5, ' times', (x, y) => {
+        return x.toString() + y;
+      });
+
+      expect(functionText).to.equal(
+        "(function (x, y) {\n        return x.toString() + y;\n      })(5, \" times\")"
       );
     });
   });
